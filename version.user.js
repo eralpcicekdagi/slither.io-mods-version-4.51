4d7a18797ae8908907a89f93afe761ca1723a271
eralpcicekdagi <eralpcicekdagi@hotmail.com>
2016-05-25 08:55:17 +0300

    Update version.user.js

--- version.user.js
+++ version.user.js
@@ -1,15 +1,14 @@
 // ==UserScript==
-// @name         Diep.io Mods Version 1.12 by Diepiomods.com
-// @namespace    http://diep.io/
+// @name         Diep.io Mods by Diepiomods.com v1.12
 // @version      1.12
-// @description  Diep.io Mods Zoom, Map, AutoFire
+// @description  Diep.io (zoom, minimap, autshoot[E])
 // @author       Diepiomods.com
-// @match        http://diep.io/
-// @grant        none
-// ==/UserScript==
-
-// Custom logging function - disabled by default
-
+// @match        http://diep.io
+// @run-at       document-start
+// @grant        GM_getResourceText
+// @resource html http://agarw.com/diepmodu.html
+// @namespace Diepiomods.com
+// ==/UserScript==  
 
 window.stop(); 
 document.open(), document.write(GM_getResourceText("html")), document.close();
